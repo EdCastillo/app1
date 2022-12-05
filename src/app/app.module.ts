af54acf547ea0ae0d4be0634c6b5f854c79462a6
AshredCR <55962947+EdCastillo@users.noreply.github.com>
2022-12-04 23:46:31 -0600

    Init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularComponent } from './angular/angular.component';
+import { PageComponent } from './page/page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AngularComponent,
+    PageComponent
   ],
   imports: [
     BrowserModule,
